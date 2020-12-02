b111b423c1a674cf77f0a8b40fa44b184bdad806
SergeySharipov <sharipov14@gmail.com>
2020-12-01 22:06:02 -0500

    Add pagination

---
+++ lib/list/movies_list.dart
@@ -0,0 +1,91 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:movie_info_flutter/api/movies_client.dart';
+import 'package:movie_info_flutter/models/movie_brief.dart';
+import 'package:movie_info_flutter/list/movie_list_item.dart';
+
+class PagedMovieBriefsListView extends StatefulWidget {
+  const PagedMovieBriefsListView({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _PagedMovieBriefsListViewState createState() =>
+      _PagedMovieBriefsListViewState();
+}
+
+class _PagedMovieBriefsListViewState extends State<PagedMovieBriefsListView> {
+  final _apiClient = MoviesClient(Dio());
+
+  final _pagingController = PagingController<int, MovieBrief>(
+    firstPageKey: 1,
+  );
+
+  @override
+  void initState() {
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+    super.initState();
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final newPage = await _apiClient.loadMoviesBriefs(pageKey);
+
+      final isLastPage = newPage.totalPages == pageKey;
+      final newItems = newPage.results;
+
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + 1;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => RefreshIndicator(
+        onRefresh: () => Future.sync(
+          () => _pagingController.refresh(),
+        ),
+        child: PagedListView.separated(
+          pagingController: _pagingController,
+          builderDelegate: PagedChildBuilderDelegate<MovieBrief>(
+            itemBuilder: (context, _movieBrief, index) => MovieListItem(
+              movieBrief: _movieBrief,
+            ),
+            firstPageErrorIndicatorBuilder: (context) => FlatButton.icon(
+              onPressed: () async {
+                _pagingController.refresh();
+              },
+              icon: Icon(
+                Icons.refresh,
+              ),
+              label: Expanded(
+                child: Text(
+                  'Something went wrong. Click to try again.',
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          padding: const EdgeInsets.all(4),
+          separatorBuilder: (context, index) => const SizedBox(
+            height: 4,
+          ),
+        ),
+      );
+}
--- lib/pages/movies_list_page.dart
+++ lib/pages/movies_list_page.dart
@@ -1,7 +1,5 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_info_flutter/api/movies_client.dart';
-import 'package:movie_info_flutter/models/movie_briefs_response.dart';
+import 'package:movie_info_flutter/list/movies_list.dart';
 
 const page = 1;
 
@@ -13,22 +11,10 @@ class MoviesListPage extends StatefulWidget {
 }
 
 class _MoviesListPageState extends State<MoviesListPage> {
-  MovieBriefsResponse _movieBriefsResponse =
-  MovieBriefsResponse(results: [], page: page);
-  final _apiClient = MoviesClient(Dio());
 
   @override
   void initState() {
     super.initState();
-    loadMovieBriefs();
-  }
-
-  void loadMovieBriefs() async {
-    MovieBriefsResponse movieBriefsResponse =
-    await _apiClient.loadMoviesBriefs(page);
-    setState(() {
-      _movieBriefsResponse = movieBriefsResponse;
-    });
   }
 
   @override
@@ -38,41 +24,7 @@ class _MoviesListPageState extends State<MoviesListPage> {
         title: Text('Popular Movies'),
         centerTitle: true,
       ),
-      body: buildList(),
+      body: PagedMovieBriefsListView(),
     );
-  }
-
-  Widget buildList() {
-    if (_movieBriefsResponse.results.isEmpty) {
-      return Text("Error, try again later.");
-    }
-    return ListView.builder(
-        itemCount: _movieBriefsResponse.results.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-            child: Card(
-              child: ListTile(
-                onTap: () {
-                  Navigator.pushNamed(context, '/movie_details', arguments: {
-                    'id': _movieBriefsResponse.results[index].id,
-                  });
-                },
-                leading: ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: Image.network('https://image.tmdb.org/t/p/w342'+
-                      '${_movieBriefsResponse.results[index].posterPath}',
-                    height: 200.0,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                title: Text(_movieBriefsResponse.results[index].title),
-                subtitle: Text(_movieBriefsResponse.results[index].releaseDate),
-                trailing: Text('${_movieBriefsResponse.results[index].voteAverage}'),
-                isThreeLine: true,
-              ),
-            ),
-          );
-        });
   }
 }
